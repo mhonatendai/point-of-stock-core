ef9c168992c9665197775eccdf765dd24a490a52
Tendai E. Mhona <mhonatendai@gmail.com>
2024-11-24 17:20:43 -0500

    Initial commit

---
+++ src/test/java/com/tenstech/shopper/ShopperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenstech.shopper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
