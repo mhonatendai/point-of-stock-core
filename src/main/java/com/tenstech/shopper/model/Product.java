95935366c0af1052fbce5932c1b1439a096fe067
Tendai E. Mhona <mhonatendai@gmail.com>
2024-12-15 02:26:15 -0500

    Refactoring

--- src/main/java/com/tenstech/shopper/model/Product.java
+++ src/main/java/com/tenstech/shopper/model/Product.java
@@ -2,7 +2,6 @@ package com.tenstech.shopper.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
