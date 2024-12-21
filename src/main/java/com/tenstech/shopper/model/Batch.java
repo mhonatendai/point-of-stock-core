98cdea6960eeb0453833b2f8d37f21648739bb4c
Tendai E. Mhona <mhonatendai@gmail.com>
2024-12-21 00:02:48 -0500

    Updating batch

--- src/main/java/com/tenstech/shopper/model/Batch.java
+++ src/main/java/com/tenstech/shopper/model/Batch.java
@@ -1,11 +1,13 @@
 package com.tenstech.shopper.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
@@ -13,8 +15,22 @@ import java.time.LocalDate;
 @Setter
 public class Batch extends AuditedEntity{
 
+    @Column
+    private String batchNumber;
+
+    @Column
+    private LocalDate batchReceivedDate;
+
+    @Column
+    private String batchStatus;
+
+    @Column
+    private BigDecimal batchPrice;
+
+    @Column
     private int quantity;
 
+    @Column
     private LocalDate expiryDate;
 
     @ManyToOne
