56514a619bcc820a8f42801e62d63af3d5b9f00d
tendai <mhonatendai@gmail.com>
2025-04-10 01:35:11 -0400

    Added builder to ApiResponse and try catch in login api

--- src/main/java/com/tenstech/pointofstock/common/utils/ApiResponse.java
+++ src/main/java/com/tenstech/pointofstock/common/utils/ApiResponse.java
@@ -1,10 +1,12 @@
 package com.tenstech.pointofstock.common.utils;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Builder
 public class ApiResponse <T>{
 
     private T data;
--- src/main/java/com/tenstech/pointofstock/user/UserController.java
+++ src/main/java/com/tenstech/pointofstock/user/UserController.java
@@ -6,6 +6,7 @@ import com.tenstech.pointofstock.exception.BusinessException;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -24,25 +25,34 @@ public class UserController {
 
     @PostMapping("/register")
     public ResponseEntity<UserDto> registerUser(@RequestBody @Validated UserDto userDto) {
-        return ResponseEntity.ok(userService.registerUser(userDto).orElseThrow( () -> new BusinessException(BusinessErrorCodes.GENERAL_EXCEPTION)));
+        return ResponseEntity.ok(userService.registerUser(userDto).orElseThrow(() -> new BusinessException(BusinessErrorCodes.GENERAL_EXCEPTION)));
     }
 
     @PostMapping("/login")
     public ResponseEntity<ApiResponse<UserDto>> loginUser(@RequestBody @Validated LoginDTO loginDTO) {
 
-        log.info("Attempting to login at {}", LocalDateTime.now());
-        UserDto authenticateUser = userService.authenticateUser(loginDTO.getUsername(), loginDTO.getPassword());
-        String apiMessage = authenticateUser != null ? "Login successful" : "Invalid username or password.";
+        try {
 
-        ApiResponse<UserDto> apiResponse = new ApiResponse<>();
-        apiResponse.setData(authenticateUser);
-        apiResponse.setApiMessage(apiMessage);
+            log.info("Attempting to login at {}", LocalDateTime.now());
+            UserDto authenticatedUser = userService.authenticateUser(loginDTO.getUsername(), loginDTO.getPassword());
+            return ResponseEntity.ok(ApiResponse.<UserDto>builder()
+                    .data(authenticatedUser)
+                    .apiMessage("Login successful")
+                    .build());
+        } catch (BadCredentialsException e) {
 
-        if (authenticateUser != null) {
-            apiResponse.setData(authenticateUser);
-            return ResponseEntity.ok(apiResponse);
-        } else {
-            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(apiResponse);
+            log.warn("Login failed for user: {} due to invalid credentials.", loginDTO.getUsername());
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED)
+                    .body(ApiResponse.<UserDto>builder()
+                            .apiMessage("Invalid username or password.")
+                            .build());
+        } catch (Exception e) {
+
+            log.error("An unexpected error occurred during login for user: {}", loginDTO.getUsername(), e);
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(ApiResponse.<UserDto>builder()
+                            .apiMessage("An unexpected error occurred during login.")
+                            .build());
         }
     }
 }
