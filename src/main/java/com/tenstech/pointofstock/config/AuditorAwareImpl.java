aeec6cab049e8159c417ebab1aa9cefd2c6dff53
tendai <mhonatendai@gmail.com>
2025-04-09 20:29:11 -0400

    Auditor Aware null bug

--- src/main/java/com/tenstech/pointofstock/config/AuditorAwareImpl.java
+++ src/main/java/com/tenstech/pointofstock/config/AuditorAwareImpl.java
@@ -5,6 +5,7 @@ import com.tenstech.pointofstock.user.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.domain.AuditorAware;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import java.util.Optional;
@@ -17,8 +18,13 @@ public class AuditorAwareImpl implements AuditorAware<String> {
  
     @Override
     public Optional<String> getCurrentAuditor() {
-        String currentUsername = SecurityContextHolder.getContext().getAuthentication().getName();
-        Optional<User> optionalUser = userService.getUserByUsername(currentUsername);
-        return optionalUser.isPresent() ? Optional.of(currentUsername) : Optional.of("system");
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication != null && authentication.isAuthenticated() && authentication.getName() != null) {
+            String currentUsername = authentication.getName();
+            Optional<User> optionalUser = userService.getUserByUsername(currentUsername);
+            return optionalUser.isPresent() ? Optional.of(currentUsername) : Optional.empty();
+        }
+        return Optional.of("system");
     }
 }
